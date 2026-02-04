f75bb4f7dd57b5aee0685fb2fea95566ead7d592
Arshath <elyasarshath6963gmail.com>
2026-02-04 21:05:20 +0530

    remove models/user.js email1

--- backo/models/user.js
+++ backo/models/user.js
@@ -37,15 +37,6 @@ const UserSchema = new mongoose.Schema(
   { timestamps: true }
 );
 
-// Prevent duplicate email error on Google users
-UserSchema.index(
-  { email: 1 },
-  {
-    unique: true,
-    partialFilterExpression: {
-      email: { $exists: true, $ne: null },
-    },
-  }
-);
+
 
 module.exports = mongoose.model('User', UserSchema);
